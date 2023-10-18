32650cf7d886620aa50958dfdf47d13b85eaaec5
Michael Kolupaev <michael.kolupaev@clickhouse.com>
2023-10-18 19:18:08 +0000

    Fix

--- src/Disks/IO/CachedOnDiskReadBufferFromFile.cpp
+++ src/Disks/IO/CachedOnDiskReadBufferFromFile.cpp
@@ -1177,7 +1177,7 @@ size_t CachedOnDiskReadBufferFromFile::getTotalSizeToRead()
 
 void CachedOnDiskReadBufferFromFile::setReadUntilPosition(size_t position)
 {
-    if (!allow_seeks_after_first_read)
+    if (initialized && !allow_seeks_after_first_read)
         throw Exception(ErrorCodes::LOGICAL_ERROR, "Method `setReadUntilPosition()` not allowed");
 
     if (read_until_position == position)
